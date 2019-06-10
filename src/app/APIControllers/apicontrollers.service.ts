31109d2cc8d954387606d643870e4b0659bc9ea7
pedrok <pedroltr89@gmail.com>
2019-06-09 21:33:07 -0300

    vinculacion de formulario de marketing con la base de datos

--- src/app/APIControllers/apicontrollers.service.ts
+++ src/app/APIControllers/apicontrollers.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+import {Persona, Consulta } from 'src/app/bd/bd.component';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -21,10 +24,21 @@ export class APIControllersService {
   ObetenerCombosDisponibles(ciudad: String){    
     return this.conector.get<any[]>(`http://localhost:3000/API/Combos/${ciudad}`);
   }
-  ObetenerPersona(email: String){    
+  ObtenerPersona(email: String){    
     return this.conector.get<any[]>(`http://localhost:3000/API/Personas/${email}`);
   }
   BuscarBeneficiosDisponibles(){    
     return this.conector.get<any[]>(`http://localhost:3000/API/Beneficios/`);
+  }
+
+  /// metodos necesarios para comunicarse con el Dpto de Marketing
+  existePersona(email: String): Boolean{
+    
+  }
+  guardarPersona(persona: Persona){
+
+  }
+  guardarConsulta(consulta: Consulta){
+
   }
 }
