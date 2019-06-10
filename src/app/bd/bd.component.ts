2bfc1743f23b1719b5fa87cf1735508ce52f6a9a
cnvale1998 <cnvale1998@gmail.com>
2019-06-09 23:44:40 -0300

    clase de Pedro copiada, git se puso loco

--- src/app/bd/bd.component.ts
+++ src/app/bd/bd.component.ts
@@ -236,3 +236,83 @@ export class Cartelera {
   
 
 }
+
+//#### PARTE DE MARKETING #######
+export class Consulta{
+  private  id: Number;
+  private motivo: String;
+  private consulta: String;
+  private email: String;
+
+  constructor(id: Number, motivo: String, consulta: String, email: String){
+     this.id = id;
+     this.email = email;
+     this.motivo = motivo;
+     this.consulta = consulta;
+  }
+
+
+  public getId(){ return this.id;  }
+  public getEmail(){return this.email;}
+  public getConsulta(){return this.consulta;}
+  public getMotivo(){return this.motivo;}
+
+  public setId(id: Number){this.id = id;}
+  public setEmail(email: String){this.email = email;}
+  public setMotivo(motivo : String){ this.motivo = motivo;}
+  public setConsulta(consulta: String){this.consulta = consulta;}
+
+}
+
+export class Persona{
+  private documento: Number;
+  private tipoDoc: String;
+  private nombre: String;
+  private apellido: String;
+  private email: String;
+  private fecha_nac: Date;
+  private telefono: String;
+  private tipo_persona: String;
+  private ciudad: String;
+
+
+  constructor( documento: Number,tipoDoc: String, nombre: String, apellido: String,email: String, fecha_nac: Date
+,telefono: String,tipo_persona: String){
+
+  this.documento = documento;
+  this.tipoDoc = tipoDoc;
+  this.nombre = nombre;
+  this.apellido = apellido;
+  this.email = email;
+  this.fecha_nac = fecha_nac;
+  this.telefono = telefono;
+  this.tipo_persona = tipo_persona;
+
+
+}
+
+public getDocumento(){return this.documento;}
+public getTipoDoc(){return this.tipoDoc;}
+public getNombre(){return this.nombre;}
+public getApellido(){return this.apellido;}
+public getEmail(){return this.email;}
+public getTelefono(){return this.telefono;}
+public getFechaNac(){return this.fecha_nac;}
+public getTipoPersona(){return this.tipo_persona;}
+public getCiudad(){return this.ciudad};
+
+public setCiudad(ciudad: String){this.ciudad =  ciudad};
+public setDocumento(documento: Number){ this.documento = documento;}
+public setTipoDoc(tipoDoc: String){ this.tipoDoc= tipoDoc;}
+public setNombre(nombre: String){ this.nombre = nombre;}
+public setApellido(apellido: String){ this.apellido = apellido;}
+public setEmail(email: String){ this.email = email;}
+public setTelefono(telefono: String){this.telefono = telefono;}
+public setFechaNac(fecha: Date){this.fecha_nac = fecha;}
+public setTipoPersona(tipo: String){ this.tipo_persona = tipo;}
+
+
+
+
+
+}
