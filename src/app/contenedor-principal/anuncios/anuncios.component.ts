7cea933760450bf976ebf84a8964b2a011d62d08
cnvale1998 <cnvale1998@gmail.com>
2019-07-13 06:50:39 -0300

    pequeñas modificaciones para precios y proximos estrenos

--- src/app/contenedor-principal/anuncios/anuncios.component.ts
+++ src/app/contenedor-principal/anuncios/anuncios.component.ts
@@ -1,4 +1,5 @@
 import { Component, AfterViewInit } from '@angular/core';
+import { APIControllersService } from 'src/app/APIControllers/apicontrollers.service';
 declare var $: any;
 @Component({
   selector: 'app-anuncios',
@@ -6,11 +7,36 @@ declare var $: any;
   styleUrls: ['./anuncios.component.css']
 })
 export class AnunciosComponent implements AfterViewInit  {
-  
-  constructor() { }
+  private hoy=new Date();
+  private peliculas:any[];
+  constructor(private conector:APIControllersService) {
+    this.conector.ObtenerCartelera(this.calcularPeriodo(this.hoy)).subscribe(res => {this.peliculas=res;});
 
+  }
+
+  calcularPeriodo(hoy:Date):String{
+    let diaSemana:number=hoy.getDay();
+    let diasRestantes:number;
+    let fechaFin:Date=new Date();
+    let fecha:String;
+    if (diaSemana<=3){
+      diasRestantes=3-diaSemana;
+      //console.log(diaSemana + "restante: "+diasRestantes);
+    }
+    else{
+      diasRestantes=10-diaSemana;
+      //console.log(diaSemana + "restante: "+diasRestantes);
+    }
+    fechaFin.setDate(hoy.getDate()+diasRestantes);
+    fecha=fechaFin.getFullYear()+"-"+(fechaFin.getMonth()+1)+"-"+fechaFin.getDate();
+    return fecha;
+  }
    ngAfterViewInit() {
       $('#myCarousel').carousel({  cycle: true });
     }
+
+verPelicula(){
+  console.log("metodo");
+}
 
 }
--- src/app/contenedor-principal/barraPrincipal/precios/precios.component.ts
+++ src/app/contenedor-principal/barraPrincipal/precios/precios.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./precios.component.css']
 })
 export class PreciosComponent implements OnInit {
-
+  private precio3D_promo:number=170;
+  private precio2D_promo:number=160;
+  private precio3D_general:number=250;
+  private precio2D_general:number=240;
   constructor() { }
 
   ngOnInit() {
